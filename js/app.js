c3cb771ea5d33c237c4e14094044a166b6997577
Jojo <katjajohanna@outlook.com>
2016-01-29 19:32:24 +0200

    Make bugs slow down when they are on the rock and get destroyed if they are under the rock

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ var Enemy = function(x_location, y_location, speed) {
     this.x = x_location;
     this.y = y_location;
     this.speed = speed;
+    this.width = 100;
+    this.height = 70;
 };
 
 // Update the enemy's position, required method for game
@@ -24,8 +26,6 @@ var Enemy = function(x_location, y_location, speed) {
 Enemy.prototype.update = function(dt) {
     this.real_x = this.x;
     this.real_y = this.y + 75;
-    this.width = 100;
-    this.height = 70;
     this.right = this.real_x + this.width;
     this.left = this.real_x;
     this.top = this.real_y;
@@ -34,17 +34,25 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
+    if (is_on_rock(this.left, this.right, this.real_y)) {
+        this.x += (this.speed * dt) / 2;
+    } else {
+        this.x += this.speed * dt;
+    }
 
     if (this.x > 505) {
-        this.x = this.initial_x;
-
-        if (this.x > -100) {
-            this.x = this.x - 505;
-        }
+        this.moveToBeginning();
     }
 
     this.checkCollision();
+};
+
+Enemy.prototype.moveToBeginning = function() {
+    this.x = this.initial_x;
+
+    if (this.x > -100) {
+        this.x = this.x - 505;
+    }
 };
 
 Enemy.prototype.checkCollision = function() {
@@ -59,6 +67,10 @@ Enemy.prototype.checkCollision = function() {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.destroy = function() {
+    this.moveToBeginning();
 };
 
 // Now write your own player class
@@ -129,6 +141,47 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
+var Victory = function() {
+    this.sprite = 'images/Star.png';
+    this.x = -200;
+    this.y = -200;
+    this.height = 171;
+};
+
+Victory.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    ctx.font = 'bold 36pt Helvetica';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'rgba(249, 243, 32, 0.7)';
+    ctx.strokeStyle = 'rgba(243, 132, 19, 0.6)';
+    ctx.lineWidth = 2;
+
+    win_text = 'You win!';
+    ctx.fillText(win_text, 505 / 2, this.y + this.height + 20);
+    ctx.strokeText(win_text, 505 / 2, this.y + this.height + 20);
+};
+
+var Rock = function(x, y) {
+    this.sprite = 'images/Rock.png';
+    this.x = x;
+    this.y = y - 30;
+    this.initial_y = this.y;
+};
+
+Rock.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Rock.prototype.update = function(dt) {
+    if (this.y > this.initial_y - 55) {
+        this.y -= 80 * dt;
+        this.real_y = this.y + 65;
+
+        destroy_enemies(this.x, this.y);
+    }
+}
+
 function is_rock_blocking_top(x, y) {
     for (var i = 0; i < rocks.length; i++) {
         y_difference = y - rocks[i].real_y;
@@ -145,7 +198,7 @@ function is_rock_blocking_bottom(x, y) {
     for (var i = 0; i < rocks.length; i++) {
         y_difference = rocks[i].real_y - y;
 
-        if (rocks[i].x == x && y_difference < 150 && y_difference > 20) {
+        if (rocks[i].x == x && y_difference < 140 && y_difference > 20) {
             return true;
         }
     }
@@ -183,42 +236,29 @@ function is_rock_blocking_right(x, y) {
     return false;
 };
 
-var Victory = function() {
-    this.sprite = 'images/Star.png';
-    this.x = -200;
-    this.y = -200;
-    this.height = 171;
-};
+function is_on_rock(left, right, y) {
+    for (var i = 0; i < rocks.length; i++) {
+        y_difference = rocks[i].real_y - y;
 
-Victory.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        if (y_difference > -40 && y_difference < 40 &&
+            right >= rocks[i].x && left <= rocks[i].x + block_width
+        ) {
+            return true;
+        }
+    }
 
-    ctx.font = 'bold 36pt Helvetica';
-    ctx.textAlign = 'center';
-    ctx.fillStyle = 'rgba(249, 243, 32, 0.7)';
-    ctx.strokeStyle = 'rgba(243, 132, 19, 0.6)';
-    ctx.lineWidth = 2;
+    return false;
+}
 
-    win_text = 'You win!';
-    ctx.fillText(win_text, 505 / 2, this.y + this.height + 20);
-    ctx.strokeText(win_text, 505 / 2, this.y + this.height + 20);
-};
+function destroy_enemies(x, y) {
+    for (var i = 0; i < allEnemies.length; i++) {
+        y_difference = allEnemies[i].real_y - y;
 
-var Rock = function(x, y) {
-    this.sprite = 'images/Rock.png';
-    this.x = x;
-    this.y = y - 30;
-    this.initial_y = this.y;
-};
-
-Rock.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Rock.prototype.update = function(dt) {
-    if (this.y > this.initial_y - 55) {
-        this.y -= 80 * dt;
-        this.real_y = this.y + 65;
+        if (y_difference > 10 && y_difference < 50 &&
+            allEnemies[i].right >= x && allEnemies[i].left <= x + block_width
+        ) {
+            allEnemies[i].destroy();
+        }
     }
 }
 
